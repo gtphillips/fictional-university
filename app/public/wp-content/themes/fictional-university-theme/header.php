9a108d9635a35ef937e5c3709c5432c2f05a8611
Greg <greg-phillips@live.co.uk>
2018-07-31 16:33:20 +0100

    Updated home and nav text

--- app/public/wp-content/themes/fictional-university-theme/header.php
+++ app/public/wp-content/themes/fictional-university-theme/header.php
@@ -18,13 +18,13 @@
 	            <li <?php if (is_page('about-us') or wp_get_post_parent_id(0) == 11) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
 	            <li <?php if(get_post_type() == 'program') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('program'); ?>">Programs</a></li>
 	            <li <?php if(get_post_type() == 'event' OR is_page('past-events')) echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
-	            <li <?php if(get_post_type() == 'campus') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('campus'); ?>">Campuses</a></li>
+	            <li <?php if(get_post_type() == 'campus') echo 'class="current-menu-item"' ?>><a href="<?php echo get_post_type_archive_link('campus'); ?>">Locations</a></li>
 	            <li <?php if(get_post_type() == 'post') echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/blog'); ?>">Blog</a></li>
 	          </ul>
 	        </nav>
 	        <div class="site-header__util">
 	          <?php if(is_user_logged_in()){?>
-	          	
+
 	          	<a href="<?php echo esc_url(site_url('/my-notes')); ?>" class="btn btn--small btn--orange float-left push-right">My Notes</a>
 
 	          	<a href="<?php echo wp_logout_url();?>" class="btn btn--small  btn--dark-orange float-left btn--with-photo">
